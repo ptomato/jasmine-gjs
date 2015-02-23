b14fee6969cde61e768ed71558a1651a23392c11
Philip Chimento <philip.chimento@gmail.com>
2015-02-23 00:00:37 -0800

    Skip focusedSpecIntegrationTest by default
    This is much cleaner than checking for the RUN_THOROUGH_TESTS environment
    variable. The test suite is simply skipped in the configuration file.
    
    Note that we still need the environment variable to prevent the 9 second
    test from running in jasmineIntegrationTest.js.

--- test/focusedSpecIntegrationTest.js
+++ test/focusedSpecIntegrationTest.js
@@ -5,41 +5,38 @@ const GLib = imports.gi.GLib;
 //   http://jasmine.github.io/2.1/focused_specs.html
 
 // By definition, this suite will disable all other suites that are run during
-// the same invocation of Jasmine -- so it is skipped by default. Otherwise you
-// couldn't run the whole test suite from the command line easily using
-// "jasmine test". To run this test anyway, define an environment variable
-// RUN_THOROUGH_TESTS=yes. During "make check" that is turned on by default,
+// the same invocation of Jasmine -- so it is skipped by default. To run this
+// test anyway, run it explicitly with "jasmine
+// test/focusedSpecIntegrationTest.js". During "make check" it is still run,
 // since make runs the spec files one by one and so this suite won't interfere
 // with any other suites.
 
-if (GLib.getenv('RUN_THOROUGH_TESTS') === 'yes') {
-    describe('Focused specs', function () {
-        fit('is focused and will run', function () {
+describe('Focused specs', function () {
+    fit('is focused and will run', function () {
+        expect(true).toBeTruthy();
+    });
+
+    it('is not focused and will not run', function () {
+        expect(true).toBeFalsy();
+    });
+
+    fdescribe('focused describe', function () {
+        it('will run', function () {
+            expect(true).toBeTruthy();
+        });
+
+        it('will also run', function () {
+            expect(true).toBeTruthy();
+        });
+    });
+
+    fdescribe('another focused describe', function () {
+        fit("is focused and will run", function () {
             expect(true).toBeTruthy();
         });
 
         it('is not focused and will not run', function () {
             expect(true).toBeFalsy();
         });
-
-        fdescribe('focused describe', function () {
-            it('will run', function () {
-                expect(true).toBeTruthy();
-            });
-
-            it('will also run', function () {
-                expect(true).toBeTruthy();
-            });
-        });
-
-        fdescribe('another focused describe', function () {
-            fit("is focused and will run", function () {
-                expect(true).toBeTruthy();
-            });
-
-            it('is not focused and will not run', function () {
-                expect(true).toBeFalsy();
-            });
-        });
     });
-}
+});
