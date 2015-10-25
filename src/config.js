fc7219f5f11324adb9b75ef066dc9873c129cfc7
Philip Chimento <philip.chimento@gmail.com>
2015-10-25 16:36:11 -0700

    Split out config functions into own module
    This is the start of a refactor that will separate the Jasmine binary
    from an internal launcher program that will actually run the tests under
    the requested environment without reading the config file twice.
    
    We split the config-related functions from command.js out into config.js
    and add ensureArray() to utils.js since it is used from both modules.
    
    We take the opportunity to add tests to functions that didn't previously
    have any test coverage.

---
+++ src/config.js
@@ -0,0 +1,76 @@
+/* global jasmineImporter */
+/* exported configToArgs, loadConfig */
+
+const Format = imports.format;
+const Gio = imports.gi.Gio;
+const GLib = imports.gi.GLib;
+
+String.prototype.format = Format.format;
+
+const Utils = jasmineImporter.utils;
+
+function _makePathsAbsolute(configFile, paths) {
+    return paths.map((path) => {
+        if (GLib.path_is_absolute(path))
+            return path;
+        return configFile.get_parent().resolve_relative_path(path).get_path();
+    });
+}
+
+function loadConfig(configFilePath) {
+    let configFile = Gio.File.new_for_commandline_arg(configFilePath);
+    let config = {};
+
+    try {
+        let [, contents] = configFile.load_contents(null);
+        if (contents instanceof Uint8Array) {
+            contents = imports.byteArray.toString(contents)
+        }
+        config = JSON.parse(contents);
+    } catch (e) {
+        throw new Error('Configuration not read from ' + configFile.get_path());
+    }
+
+    if (config.include_paths)
+        config.include_paths = _makePathsAbsolute(configFile,
+            Utils.ensureArray(config.include_paths));
+    if (config.spec_files)
+        config.spec_files = _makePathsAbsolute(configFile,
+            Utils.ensureArray(config.spec_files));
+
+    const RECOGNIZED_KEYS = [
+        'environment',
+        'exclude',
+        'include_paths',
+        'options',
+        'spec_files',
+    ];
+    Object.keys(config).forEach((key) => {
+        if (RECOGNIZED_KEYS.indexOf(key) === -1)
+            printerr('warning: unrecognized config file key "%s"'.format(key));
+    });
+
+    print('Configuration loaded from', configFile.get_path());
+    return config;
+}
+
+function configToArgs(config, shouldAddSpecFiles=false) {
+    let retval = [];
+    if (config.include_paths) {
+        Utils.ensureArray(config.include_paths).forEach((path) => {
+            retval.push('-I');
+            retval.push(path);
+        });
+    }
+    if (config.exclude) {
+        Utils.ensureArray(config.exclude).forEach((exclude) => {
+            retval.push('--exclude');
+            retval.push(exclude);
+        });
+    }
+    if (config.options)
+        retval = retval.concat(Utils.ensureArray(config.options));
+    if (shouldAddSpecFiles && config.spec_files)
+        retval = retval.concat(Utils.ensureArray(config.spec_files));
+    return retval;
+}
