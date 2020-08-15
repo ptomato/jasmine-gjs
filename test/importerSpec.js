498c83cd1d10c6f8d4f803e943fda57350edc07b
Philip Chimento <philip.chimento@gmail.com>
2020-08-15 11:45:48 -0700

    Suppress a few more undefined property warnings
    This gets rid of SpiderMonkey's strict warnings about undefined
    properties when running Jasmine.

--- test/importerSpec.js
+++ test/importerSpec.js
@@ -2,7 +2,7 @@ const Utils = imports.utils;
 
 describe('Jasmine importer', function () {
     it('hides Jasmine modules from the test code', function () {
-        expect(Utils.indent).not.toBeDefined();
+        expect(Object.keys(Utils)).not.toContain('indent');
     });
 
     it("lets test code import modules named the same as Jasmine's", function () {
