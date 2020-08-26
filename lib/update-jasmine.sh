c355872b53d7a78f4e1570b595e3426dca6d0187
Andy Holmes <andrew.g.r.holmes@gmail.com>
2020-08-26 01:12:37 -0700

    Update to jasmine-2.3.4
    This is the latest patch release from the 2.3.x series

--- lib/update-jasmine.sh
+++ lib/update-jasmine.sh
@@ -1,5 +1,5 @@
 # This script is used to build our custom version of the Jasmine library by
 # downloading the upstream file and applying our patches, and is meant for use
 # only when upgrading to a new upstream version.
-curl -o jasmine.js https://raw.githubusercontent.com/jasmine/jasmine/v2.3.0/lib/jasmine-core/jasmine.js
+curl -o jasmine.js https://raw.githubusercontent.com/jasmine/jasmine/v2.3.4/lib/jasmine-core/jasmine.js
 patch <local-modifications.patch jasmine.js
