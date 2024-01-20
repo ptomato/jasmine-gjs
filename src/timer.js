bcfa7364af3990f5114d791f7abffb95cfb4f52a
Florian Müllner <fmuellner@gnome.org>
2024-01-20 14:09:52 +0100

    timer: Don't install setTimeout/setInterval API
    It's already provided by gjs nowadays, and has been for nearly
    two years.

--- src/timer.js
+++ src/timer.js
@@ -1,27 +1,6 @@
-/* exported installAPI, createDefaultTimer */
+/* exported createDefaultTimer */
 
 const {GLib} = imports.gi;
-const Mainloop = imports.mainloop;
-
-function _setTimeoutInternal(continueTimeout, func, time) {
-    return Mainloop.timeout_add(time, function () {
-        func();
-        return continueTimeout;
-    });
-}
-
-function _clearTimeoutInternal(id) {
-    if (id > 0)
-        Mainloop.source_remove(id);
-}
-
-// Installs the browser setTimeout/setInterval API on the given object. (Pass
-// globalThis to get these available globally.)
-function installAPI(global) {
-    global.setTimeout = _setTimeoutInternal.bind(undefined, GLib.SOURCE_REMOVE);
-    global.setInterval = _setTimeoutInternal.bind(undefined, GLib.SOURCE_CONTINUE);
-    global.clearTimeout = global.clearInterval = _clearTimeoutInternal;
-}
 
 // Measures elapsed time in milliseconds.
 function createDefaultTimer() {
