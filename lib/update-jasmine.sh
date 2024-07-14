06d8ee3e291a0f25231669c67e4a961a8e836668
Philip Chimento <philip.chimento@gmail.com>
2024-07-14 15:17:33 -0700

    Remove local patches to jasmine.js
    These were all added to suppress warnings that no longer exist.

--- lib/update-jasmine.sh
+++ lib/update-jasmine.sh
@@ -2,4 +2,4 @@
 # downloading the upstream file and applying our patches, and is meant for use
 # only when upgrading to a new upstream version.
 curl -o jasmine.js https://raw.githubusercontent.com/jasmine/jasmine/v3.10.1/lib/jasmine-core/jasmine.js
-patch <local-modifications.patch jasmine.js
+# patch <local-modifications.patch jasmine.js
